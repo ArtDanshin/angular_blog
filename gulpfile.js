2d426bb3e2f25e7f093070aa12d715d6fe4c002d
Artem Danshin <a.danshin@rambler-co.ru>
2016-02-23 15:22:43 +0300

    Initial Init - Starting pattern completed

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+'use strict'
+
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	scss = require('gulp-sass'),
+	uglify = require('gulp-uglify'),
+	ngAnnotate = require('gulp-ng-annotate'),
+	webserver = require('gulp-webserver');
+
+gulp.task('libsjs', function(){
+	gulp.src([
+		'app/bower/angular/angular.js',
+		'app/bower/angular-route/angular-route.js'
+		])
+	.pipe(concat('libs.js'))
+	.pipe(gulp.dest('builds/dev'))
+});
+
+gulp.task('js', function(){
+	gulp.src([
+		'app/js/**/*.js'
+	])
+	.pipe(concat('app.js'))
+	.pipe(gulp.dest('builds/dev'));
+});
+
+gulp.task('scss', function(){
+	gulp.src([
+		'app/scss/**/*.scss'
+	])
+	.pipe(scss())
+	.pipe(concat('app.css'))
+	.pipe(gulp.dest('builds/dev'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('app/js/**/*.js', ['js']);
+	gulp.watch('app/scss/**/*.scss', ['scss']);
+});
+
+gulp.task('webserver', function(){
+	gulp.src('builds/dev')
+	.pipe(webserver({
+		livereload: true,
+		open: true
+	}));
+});
+
+gulp.task('default', [
+	'libsjs',
+	'js',
+	'scss',
+	'watch',
+	'webserver'
+]);
