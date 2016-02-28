1f5d67eb62b16cef360b7e2f0f8419eaaed18d91
SilverDragoon <silvir007@gmail.com>
2016-02-28 16:47:44 +0300

    Build

--- builds/dev/app.js
+++ builds/dev/app.js
@@ -1,23 +1,3 @@
-;(function(){
-	'use strict';
+!function(){"use strict";function o(){console.log("ag_blog::config")}function n(o){console.log("ag_blog::run")}angular.module("ag_blog",["ag_blog.users","ui.router"]).config(o).run(n),n.$inject=["$rootScope"]}();
 
-	angular
-		.module('ag_blog', ['ag_blog.users'])
-		.config(function(){
-			console.log('ag_blog::config');
-		})
-		.run(function(){
-			console.log('ag_blog::run');
-		})
-})();
-
-;(function(){
-	'use strict';
-
-	angular.module('ag_blog.users', [])
-		.controller('MainCtrl', ['$scope', function ($scope) {
-			console.log('ng5::MainCtrl $scope=', $scope);
-			$scope.message = 'Привет';
-			this.message = 'Привет от this!';
-		}])
-})();
+!function(){"use strict";angular.module("ag_blog.users",["ui.router"]).controller("MainCtrl",["$scope",function(o){console.log("ng5::MainCtrl $scope=",o),o.message="Привет",this.message="Привет от this!"}]).config(function(){})}();
