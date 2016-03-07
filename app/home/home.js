3928377100418a58499b78e3b0b98cdcbac4101a
SilverDragoon <silvir007@gmail.com>
2016-03-07 21:46:50 +0300

    Learn Singletone applications

--- app/home/home.js
+++ app/home/home.js
@@ -7,10 +7,57 @@
 	.config(configHome)
 	.controller('HomeCtrl', homeController)
 	.constant('SOME_VAL', 'Некоторе значение')
+	.service('home_service', homeService)
+	.factory('home_factory', homeFactory)
+	.provider('home_prov', homeProvider)
 
-	function homeController($scope, SOME_VAL){
+	function homeProvider() {
+		// Configure private data
+		var privateVal = 'Значение 1';
+
+		return {
+			setPrivate: function(_newVal){
+				privateVal = _newVal;
+			},
+			$get: function(){
+				function getPrivate() {
+					return privateVal;
+				}
+				return {
+					publicVal: 'Публичная переменная',
+					getPrivate: getPrivate
+				}
+			}
+		}
+	}
+
+	function homeFactory($log){
+		$log.debug('homeService init');
+	};
+
+	function homeService($log){
+		$log.debug('homeService init');
+
+		var count = 0;
+
+		this.add = function(){
+			count++;
+			return count;
+		}
+
+		this.rem = function(){
+			count--;
+			return count;
+		}
+	};
+
+	function homeController($scope, $log, SOME_VAL, home_service, home_prov){
 		var s = this;
 		s.pageName = 'home';
+
+		// $log.debug('home_service add', home_service.add(), home_service.add());
+		// $log.debug('home_service add', home_service.rem());
+		$log.debug('home_prov getProvider ', home_prov.getPrivate());
 	};
 
 	function configHome($stateProvider){
--- app/main.js
+++ app/main.js
@@ -10,8 +10,17 @@
 		.config(configAgBlog)
 		.run(runAgBlog);
 
-		function configAgBlog(){
+		function configAgBlog($urlRouterProvider, $provide){
 			console.log('ag_blog::config');
+			$urlRouterProvider.otherwise('/');
+
+			$provide.decorator('$log', function($delegate){
+				$delegate.hello = function(_m){
+					console.debug('_m', init)
+				}
+
+				return $delegate;
+			})
 		};
 
 		function runAgBlog($rootScope){
